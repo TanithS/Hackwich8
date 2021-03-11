412279a768d0ca847d46bec5cd76458259391b29
TanithS <77208045+TanithS@users.noreply.github.com>
2021-03-11 13:45:51 -1000

    Completed Problem Sets 1 & 2 for Hackwich #8

--- Hackwich8/ViewController.swift
+++ Hackwich8/ViewController.swift
@@ -5,17 +5,19 @@
 //  Created by User on 3/9/21.
 //
 
-import UIKit
+/*import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     
-    var currentValue = 0
+    var currentValue = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        currentValue = Int(slider.value)
     }
     
     
@@ -40,5 +42,75 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+}*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue = 0
+    
+    var targetValue = 0
+    
+    @IBOutlet weak var targetValueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        slider.value = 50
+        
+        currentValue = Int(slider.value)
+        
+        startNewRound()
+    }
+
+    
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        
+        print("The value of the slider is: \(slider.value)")
+        
+        currentValue = Int(slider.value)
+        
+        //targetValue = Int.random(in: 0...100)
+    
+    }
+    
+    func startNewRound(){
+        
+        targetValue = Int.random(in: 0...100)
+        
+        currentValue = Int(slider.value)
+        
+        updateTargetLabel()
+    
+    }
+
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        
+        let message = "Your guess is \(currentValue)" + "\n Your target value for this round was \(targetValue)"
+        
+        let alert = UIAlertController(title: "Guess the Number Game", message: message , preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "New Round", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    
+        
+    }
+    
+    func updateTargetLabel(){
+        
+        targetValueLabel.text = String(targetValue)
+        
+    }
 }
+
+
 
